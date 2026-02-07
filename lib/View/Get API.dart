dca4dcc04cad8b3971d861a13e6edf71320afaba
sfrabby <fazlerabby6732@gmail.com>
2026-02-07 19:53:45 +0600

    UI done

--- lib/View/Get API.dart
+++ lib/View/Get API.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class GetApi extends StatelessWidget {
   const GetApi({super.key});
@@ -8,9 +9,19 @@ class GetApi extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
-        title: Text("Api practice"),
+        title: Text("Api practice", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
       ),
-
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(child: ListTile(
+            title: Text("Hello"),
+            subtitle: Text("Price"),
+            leading: CircleAvatar(child: Text("I"),backgroundColor: Colors.teal ,),
+          ));
+        },
+      ),
     );
   }
 }
